39eda9a6a040fc6c85c2f9b159d9fcfbb4b43bb2
Diego Steiner <diego.steiner@u041.ch>
2018-02-09 13:47:18 +0100

    Update dependencies

--- src/main.js
+++ src/main.js
@@ -3,8 +3,24 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import VueFire from 'vuefire';
+import firebase from 'firebase';
 
 Vue.config.productionTip = false;
+
+// explicit installation required in module environments
+Vue.use(VueFire);
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAs9w7l1JOKHOsGBT0k1oPiT7rfRkhP4bg",
+  authDomain: "strukturen-fragebogen.firebaseapp.com",
+  databaseURL: "https://strukturen-fragebogen.firebaseio.com",
+  projectId: "strukturen-fragebogen",
+  storageBucket: "strukturen-fragebogen.appspot.com",
+  messagingSenderId: "168288290356"
+};
+firebase.initializeApp(config);
 
 /* eslint-disable no-new */
 new Vue({
